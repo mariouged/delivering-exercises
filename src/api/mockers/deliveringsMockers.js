3fff29bbc9ac879bd4f3224fb02c0298cad4b14c
Mario <mario.ugedo@gmail.com>
2023-09-25 19:02:25 +0200

    document download url

--- src/api/mockers/deliveringsMockers.js
+++ src/api/mockers/deliveringsMockers.js
@@ -6,6 +6,7 @@ export const deliveringsMockers = [
     type: 'Document / PDF',
     size: 3.8,
     delivery_date: new Date(2018, 11, 28, 14, 45),
+    download_url: '/mock-document-url.pdf',
     comments: [
       { user: 123, role: 'student', comment_date: new Date(2018, 11, 28, 14, 45), message: 'Buenas tardes, acabo de subir el primer archivo con el primer caso práctico, espero revisión.' },
       { user: 456, role: 'teacher', comment_date: new Date(2018, 11, 29, 10, 11), message: '¡Gran trabajo Silvia! Pero creo que deberías revisar el punto 3. Intenta añadir un poco más de contexto y un apartado final de conclusiones personales.' },
@@ -18,6 +19,7 @@ export const deliveringsMockers = [
     type: 'Document / PDF',
     size: 4.8,
     delivery_date: new Date(2018, 11, 30, 8, 22),
+    download_url: '/mock-document-url.pdf',
     comments: [
       { user: 123, role: 'student', comment_date: new Date(2018, 11, 30, 8, 22), message: 'Realizados los cambios según feedback.' },
       { user: 456, role: 'teacher', comment_date: new Date(2018, 11, 30, 15, 25), message: 'Perfecto excelente trabajo.' },
--- src/components/DeliveringDocument.vue
+++ src/components/DeliveringDocument.vue
@@ -5,6 +5,10 @@ import { formatDate } from '../commons/formatDate'
 const props = defineProps({
   delivering: Object,
 })
+
+function downloadDocument(download_url) {
+  window.open(encodeURI(download_url))
+}
 </script>
 
 <template>
@@ -12,7 +16,7 @@ const props = defineProps({
   <div class="py-4 flex flex-row">
     <div class="basis-full text-sm">
       <p class="font-bold">VERSIÓN {{ delivering.version }}</p>
-      <p class="text-blue-500 font-bold">{{ delivering.name }}</p>
+      <p><a href="#" @click="downloadDocument(delivering.download_url)" class="text-blue-500 font-bold" target="_blank">{{ delivering.name }}</a></p>
       <div class="flex flex-row">
         <div class="basis-32">{{ delivering.type }}</div>
         <div class="basis-24">({{ delivering.size }} MB)</div>
@@ -20,7 +24,7 @@ const props = defineProps({
       </div>
     </div>
     <div class="basis-12 pt-6">
-      <a href="#" class="text-blue-500">
+      <a href="#" @click="downloadDocument(delivering.download_url)" class="text-blue-500">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-10 h-10">
           <path fill-rule="evenodd" d="M10.5 3.75a6 6 0 00-5.98 6.496A5.25 5.25 0 006.75 20.25H18a4.5 4.5 0 002.206-8.423 3.75 3.75 0 00-4.133-4.303A6.001 6.001 0 0010.5 3.75zm2.25 6a.75.75 0 00-1.5 0v4.94l-1.72-1.72a.75.75 0 00-1.06 1.06l3 3a.75.75 0 001.06 0l3-3a.75.75 0 10-1.06-1.06l-1.72 1.72V9.75z" clip-rule="evenodd" />
         </svg>
